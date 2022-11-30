21a30413af45ca2d482169932f34de1ef0769389
Aleksandr Zakharov <Alexfang@bk.ru>
2022-11-30 21:05:29 +0300

    Полностью освоил и усвоил урок

---
+++ HEX1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 ->  max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+Console.Clear();
+Console.Write("Введите число №1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine($"В результате наибольшее значение {n} ");
+else
+    Console.WriteLine($"В результате наибольшее значение {m} ");
---
+++ HEX2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Clear();
+Console.Write("Введите число №1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №3: ");
+int z = Convert.ToInt32(Console.ReadLine());
+if (x > y || x > z)
+    Console.WriteLine($"В результате наибольшее число: {x}");
+else if (y > z || y > x)
+    Console.WriteLine($"В результате наибольшее число: {y}");
+else if (z > x || z > y)
+    Console.WriteLine($"В результате наибольшее число: {z}");
